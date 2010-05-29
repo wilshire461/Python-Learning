b59f4dd3c872e471e8460234b5099b9b5aa9889f
wilshire461 <wilshire461@gmail.com>
2010-05-29 14:13:36 -0400

    Ps1B

---
+++ SumofPrimes.py
@@ -0,0 +1,22 @@
+from math import *
+def productOfPrimes(max):
+    prime_list=[2]    
+    n=1
+    while prime_list[-1] <= max:
+        n+=2
+        for divisor in range(3,int(n**0.5)+1, 2):
+	        remainder = n % divisor
+	        if remainder == 0: break
+	else:
+	    prime_list.append(n)
+    sumOfPrimes=0
+    i=0
+    for prime in prime_list:
+        
+        sumOfPrimes+=log(prime)
+    print "Sum of Primes:",sumOfPrimes
+    ratio=sumOfPrimes/max
+    return ratio    
+
+ratio = productOfPrimes(700)
+print "Ratio:",ratio
